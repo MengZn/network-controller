63c0434d4d887baef178024d7e44a5f71fb4a2de
MengZeLee <adnt587@gmail.com>
2019-03-18 13:28:40 +0800

    fix etcdv3 init function

--- etcd/client.go
+++ etcd/client.go
@@ -3,10 +3,9 @@ package etcd
 import (
 	"context"
 	"errors"
-	"fmt"
 	"time"
 
-	etcdcv3 "github.com/coreos/etcd/clientv3"
+	etcdcv3 "go.etcd.io/etcd/clientv3"
 )
 
 const (
@@ -15,7 +14,7 @@ const (
 	etcdTimeout = 5 * time.Second
 )
 
-type Client struct {
+type EtcdClient struct {
 	PathPrefix string
 	Ctx        context.Context
 	Cli        *etcdcv3.Client
@@ -24,21 +23,16 @@ type Client struct {
 var errKeyNotFound = errors.New("key not found")
 var errParse = errors.New("parse etcd fail")
 
-func New(ep []endPoints) *Client,error{
-	cli, err := newEtcdClient(ep);
-	if err != nil {
-		return nil,err
-	}
-	return cli,nil
-}
+func New(ep []string) (*EtcdClient, error) {
+	etcdcleint := new(EtcdClient)
 
-func newEtcdClient(endpoints []string) (*etcdcv3.Client, error) {
 	etcdCfg := etcdcv3.Config{
-		Endpoints: endpoints,
+		Endpoints: ep,
 	}
-	cli, err := etcdcv3.New(etcdCfg)
+	ecli, err := etcdcv3.New(etcdCfg)
 	if err != nil {
 		return nil, err
 	}
-	return cli, nil
+	etcdcleint.Cli = ecli
+	return etcdcleint, nil
 }
